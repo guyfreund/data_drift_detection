f4eb1c2f6dafb6fe30ad54be7423e70ac1ba4359
guyfreund <guyfreund@gmail.com>
2021-12-04 20:10:46 +0200

    wow

--- src/classes/ifeature_metric.py
+++ src/classes/ifeature_metric.py
@@ -1,4 +1,4 @@
-from abc import ABS, abstractmethod
+from abc import ABS
 
 class IFeatureMetric(ABC):
   """
@@ -6,36 +6,29 @@ class IFeatureMetric(ABC):
   """
 
   @property
-  @abstractmethod
   def name(self) -> str
     raise NotImplementedError
 
   @property
-  @abstractmethod
   def dataset(self) -> str
     raise NotImplementedError
 
   @property
-  @abstractmethod
   def feature_type(self) -> str
     raise NotImplementedError
 
   @property
-  @abstractmethod
   def number_of_nulls(self) -> int
     raise NotImplementedError
 
   @property
-  @abstractmethod
   def mean(self) -> float
     raise NotImplementedError
 
   @property
-  @abstractmethod
   def variance(self) -> float
     raise NotImplementedError
 
   @property
-  @abstractmethod
   def is_important_feature(self) -> bool
     raise NotImplementedError
