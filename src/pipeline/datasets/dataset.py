ad84af26fb5d615ede579d9ab9797bf0c84a60f4
guyfreund <guyfreund@gmail.com>
2021-12-06 19:32:54 +0200

    wow

--- src/pipeline/datasets/dataset.py
+++ src/pipeline/datasets/dataset.py
@@ -14,10 +14,6 @@ class Dataset:
     self._raw_df = self.load(self._path)
     self._num_instances, self._num_features = self._df.shape
     self._dtype = dtype
-    self._preprocessed_df = None
-    self._train_df = None
-    self._validation_df = None
-    self._test_df = None
   
   @property
   def num_features(self) -> int:
@@ -38,38 +34,6 @@ class Dataset:
   @property
   def raw_df(self) -> pd.DataFrame:
     return self._raw_df
-
-  @property
-  def preprocessed_df(self) -> pd.DataFrame:
-    return self._preprocessed_df
-
-  @property
-  def train_df(self) -> pd.DataFrame:
-    return self._train_df
-
-  @property
-  def validation_df(self) -> pd.DataFrame:
-    return self._test_df
-
-  @property
-  def test_df(self) -> pd.DataFrame:
-    return self._test_df
-
-  @preprocessed_df.setter
-  def preprocessed_df(self, value: pd.DataFrame):
-    self._preprocessed_df = value
-
-  @train_df.setter
-  def train_df(self, value: pd.DataFrame):
-    self._train_df = value
-
-  @validation_df.setter
-  def validation_df(self, value: pd.DataFrame):
-    self._test_df = value
-
-  @test_df.setter
-  def test_df(self, value: pd.DataFrame):
-    self._test_df = value
 
   @abstractmethod
   def load(self) -> pd.DataFrame:
--- src/pipeline/preprocessing/interfaces/ipreprocessor.py
+++ src/pipeline/preprocessing/interfaces/ipreprocessor.py
@@ -12,4 +12,45 @@ class IPreprocessor(ABC):
 
   @abstractmethod
   def preprocess(self, dataset: Dataset) -> Tuple[pd.DataFrame, pd.DataFrame, List[IFeatureMetric]]:
+    """ preprocesses the raw dataset
+        saves the processed data frame in self._processed_df
+
+    Args:
+        dataset (Dataset): The raw dataset
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame, List[IFeatureMetric]]:
+        X (pd.DataFrame): the processesed data frame
+        X+ (pd.DataFrame): the processed data frame with the addition of the DatasetType column for all instances
+        feature_metrics_list (List[IFeatureMetric]]): a list of IFeatureMetric objects per feature
+    """
+    raise NotImplementedError
+
+  @abstractmethod
+  def split(self) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """ split the processed dataset into X_train, X_validation, X_test
+        saves the sets in self._X_train, self._X_validation, self._X_test
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+        X_train (pd.DataFrame): the training set
+        X_validation (pd.DataFrame): the validation set
+        X_test (pd.DataFrame): the test set
+    """
+    raise NotImplementedError
+
+  @property
+  def processed_df(self) -> pd.DataFrame:
+    raise NotImplementedError
+
+  @property
+  def X_train(self) -> pd.DataFrame:
+    raise NotImplementedError
+
+  @property
+  def X_validation(self) -> pd.DataFrame:
+    raise NotImplementedError
+
+  @property
+  def X_test(self) -> pd.DataFrame:
     raise NotImplementedError
