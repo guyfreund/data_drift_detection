5de7f62673ed216d0940019de99a5a31d620e6d0
guyfreund <guyfreund@gmail.com>
2021-12-10 16:45:42 +0200

    data drift model based detector

--- src/pipeline/data_drift_detection/detector/data_drift_detector.py
+++ src/pipeline/data_drift_detection/detector/data_drift_detector.py
@@ -1,11 +1,13 @@
 from typing import List
 
 from src.pipeline.data_drift_detection.interfaces.idata_drift_detector import IDataDriftDetector
+from src.pipeline.data_drift_detection.data_drift import DataDrift
 
 
 class DataDriftDetector(IDataDriftDetector):
     def __init__(self, detectors: List[IDataDriftDetector]):
         self._detectors = detectors
 
-    def detect(self) -> bool:
-        return any(detector.detect().is_drifted for detector in self._detectors)
+    def detect(self) -> DataDrift:
+        is_drifted = any(detector.detect().is_drifted for detector in self._detectors)
+        return DataDrift(is_drifted=is_drifted)
