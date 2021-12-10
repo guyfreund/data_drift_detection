4b82e8661a43368f7853f744c7bd7d171e050d7f
guyfreund <guyfreund@gmail.com>
2021-12-10 17:12:01 +0200

    wow

--- src/pipeline/data_drift_detection/detector/model_based_detector.py
+++ src/pipeline/data_drift_detection/detector/model_based_detector.py
@@ -16,25 +16,27 @@ from src.pipeline.model.constants import ModelMetricType
 class ModelBasedDetector(IDataDriftDetector):
     def __init__(self, deployment_dataset_plus: Dataset, training_processed_df_plus_path: str, preprocessor: IPreprocessor, model: IModel):
         assert os.path.exists(training_processed_df_plus_path)
-        self._training_processed_df_plus_path = training_processed_df_plus_path
-        self._deployment_dataset_plus = deployment_dataset_plus
-        self._preprocessor = preprocessor
-        self._model = model
+        self._training_processed_df_plus_path: str = training_processed_df_plus_path
+        self._deployment_dataset_plus: Dataset = deployment_dataset_plus
+        self._preprocessor: IPreprocessor = preprocessor
+        self._model: IModel = model
 
     def detect(self) -> ModelBasedDataDrift:
         # concatenate the training and deployment processed dataframes
-        training_processed_df_plus = pd.read_pickle(self._training_processed_df_plus_path)
+        training_processed_df_plus: pd.DataFrame = pd.read_pickle(self._training_processed_df_plus_path)
+        # TODO: think maybe to use pickle here
         _, deployment_processed_df_plus, _ = self._preprocessor.preprocess(dataset=self._deployment_dataset_plus)
-        processed_df = pd.concat([training_processed_df_plus, deployment_processed_df_plus])
+        processed_df: pd.DataFrame = pd.concat([training_processed_df_plus, deployment_processed_df_plus])
 
         # train and evaluate the model
         X_train, X_validation, X_test, y_train, y_validation, y_test = self._preprocessor.split(processed_df=processed_df)
+        # TODO: think maybe to use pickle here
         self._model.train(X_train=X_train, y_train=y_train)
         self._model.tune_hyperparameters(X_validation=X_validation, y_validation=y_validation)
         model_metrics_dict: Dict[ModelMetricType, IModelMetric] = self._model.evaluate(X_test=X_test, y_test=y_test)
 
         # data drift is detected if model accuracy is like a coin-flip
-        model_accuracy = model_metrics_dict[ModelMetricType.Accuracy].value
-        is_drifted = np.abs(0.5 - model_accuracy) < Config().data_drift.model_based_threshold
+        model_accuracy: float = model_metrics_dict[ModelMetricType.Accuracy].value
+        is_accuracy_like_coin_flip = np.abs(0.5 - model_accuracy) < Config().data_drift.model_based_threshold
 
-        return ModelBasedDataDrift(is_drifted=is_drifted)
+        return ModelBasedDataDrift(is_drifted=not is_accuracy_like_coin_flip)
