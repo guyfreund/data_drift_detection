0334fe2bdf2ec93abaf61fac8f49d5b97b687e82
guyfreund <guyfreund@gmail.com>
2021-12-10 20:27:15 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1'
-DESCRIPTION = 'Data Drift Detection - MLOps project, Reichman University 2022'
+DESCRIPTION = 'Data Drift Detection - MLOps project, Reichman University 2022\n'
 LONG_DESCRIPTION = 'Implementation of a Machine Learning Operations pipeline, which consists of: model training, ' \
-                   'data drift detection & data slicing with regards to fairness. Databases being used are: \n' \
-                   'https://archive.ics.uci.edu/ml/datasets/bank+marketing\n' \
-                   'https://archive.ics.uci.edu/ml/datasets/statlog+(german+credit+data)\n'
+                   'data drift detection & data slicing with regards to fairness.\n' \
+                   'Databases being used are: \n' \
+                   '- https://archive.ics.uci.edu/ml/datasets/bank+marketing\n' \
+                   '- https://archive.ics.uci.edu/ml/datasets/statlog+(german+credit+data)\n'
 
 setup(
     name="data_drift_detector",
@@ -16,11 +17,9 @@ setup(
     license="MIT",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[],  # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
+    install_requires=[],
     python_requires=">=3.10",
     url="https://github.com/guyfreund/data_drift_detctor/",
-    keywords=['python', 'first package'],
     classifiers=[
         "Intended Audience :: Education",
         "License :: OSI Approved :: MIT License"
