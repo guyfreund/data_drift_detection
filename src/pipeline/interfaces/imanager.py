a0d529a53d6a069e05bebbafcc3555af50fccbdc
guyfreund <guyfreund@gmail.com>
2021-12-12 18:08:46 +0200

    wow

--- src/pipeline/interfaces/imanager.py
+++ src/pipeline/interfaces/imanager.py
@@ -1,8 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Tuple, List, Any
-import pandas as pd
-
-from src.pipeline.data_drift_detection.interfaces.idata_drift_detector import IDataDriftDetector
+from typing import Any
 
 
 class IManager(ABC):
