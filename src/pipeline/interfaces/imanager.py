9ec409a4a70bd5f86370290f5d0f330f1753df5d
guyfreund <guyfreund@gmail.com>
2021-12-12 18:08:35 +0200

    wow

--- src/pipeline/interfaces/imanager.py
+++ src/pipeline/interfaces/imanager.py
@@ -12,20 +12,5 @@ class IManager(ABC):
 
     @abstractmethod
     def manage(self) -> Any:
-        """ preprocesses the raw dataset
-        saves the processed data frame in self._processed_df
-        saves the processed dataset as a pickle
-        saves the processed dataset plus as a pickle
-        saves feature_metrics_list as a pickle
-
-        Args:
-            dataset (Dataset): The raw dataset
-
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame, List[IFeatureMetrics]]:
-            processed_dataset (pd.DataFrame): the processed data frame
-            processed_dataset_plus (pd.DataFrame): the processed data frame with the addition of the DatasetType column for all instances
-            feature_metrics_list (List[IFeatureMetrics]): a list of IFeatureMetric objects per feature
-        """
         raise NotImplementedError
 
