d15371c925bbbf74d6f06cda2c1c3f4093b11c70
guyfreund <guyfreund@gmail.com>
2021-12-13 23:29:38 +0200

    added multiple datasets managers classes

--- src/pipeline/data_generation/data_generation_manager.py
+++ src/pipeline/data_generation/data_generation_manager.py
@@ -1,5 +1,13 @@
 from src.pipeline.interfaces.imanager import IManager
 
 
+class DataGenerationManagerInfo:
+    pass
+
+
 class DataGenerationManager(IManager):
+    pass
+
+
+class MultipleDatasetGenerationManager(IManager):
     pass
--- src/pipeline/model/model_trainining_manager.py
+++ src/pipeline/model/model_trainining_manager.py
@@ -1,5 +1,13 @@
 from src.pipeline.interfaces.imanager import IManager
 
 
+class ModelTrainingManagerInfo:
+    pass
+
+
 class ModelTrainingManager(IManager):
+    pass
+
+
+class MultipleDatasetModelTrainingManager(IManager):
     pass
