033e826edffde091aee65e2facf8ee024e45433f
guyfreund <guyfreund@gmail.com>
2021-12-16 01:45:20 +0200

    update model training manager

--- src/pipeline/model/model_trainining_manager.py
+++ src/pipeline/model/model_trainining_manager.py
@@ -23,9 +23,11 @@ class ModelTrainingManager(IManager):
     def manage(self) -> Tuple[ModelTrainingManagerInfo, List[IFeatureMetrics], Dict[ModelMetricType, IModelMetric]]:
         processed_dataset, processed_dataset_plus, feature_metrics_list = self._info.preprocessor.preprocess(self._info.training_dataset)
         X_train, X_validation, X_test, y_train, y_validation, y_test = self._info.preprocessor.split(processed_dataset)
+
         self._info.model.train(X_train, y_train)
         self._info.model.tune_hyperparameters(X_validation, y_validation)
         model_metrics_dict = self._info.model.evaluate(X_test, y_test)
+
         self._info.model.load(self._info.model.__class__.__name__)
         return self._info, feature_metrics_list, model_metrics_dict
 
@@ -42,15 +44,16 @@ class MultipleDatasetModelTrainingManager(IManager):
     def __init__(self, info_list: List[ModelTrainingManagerInfo]):
         self._model_training_managers: List[ModelTrainingManager] = [ModelTrainingManager(info) for info in info_list]
 
-    def manage(self) -> List[Tuple[ModelTrainingManagerInfo, List[IFeatureMetrics], Dict[ModelMetricType, IModelMetric]]]:
-        result: List[Tuple[ModelTrainingManagerInfo, List[IFeatureMetrics], Dict[ModelMetricType, IModelMetric]]] = \
-            [manager.manage() for manager in self._model_training_managers]
-        return result
+    def manage(self) -> Tuple[List[ModelTrainingManagerInfo], List[List[IFeatureMetrics]], List[Dict[ModelMetricType, IModelMetric]]]:
+        model_training_manger_info_list: List[ModelTrainingManagerInfo] = []
+        model_training_manager_feature_metrics_lists: List[List[IFeatureMetrics]] = []
+        model_training_model_metrics_list: List[Dict[ModelMetricType, IModelMetric]] = []
 
-    @property
-    def model_training_managers(self) -> List[ModelTrainingManager]:
-        return self._model_training_managers
+        for model_training_manager in self._model_training_managers:
+            info, feature_metrics_list, model_metrics_dict = model_training_manager.manage()
 
-    @model_training_managers.setter
-    def model_training_managers(self, model_training_managers):
-        self._model_training_managers = model_training_managers
+            model_training_manger_info_list.append(info)
+            model_training_manager_feature_metrics_lists.append(feature_metrics_list)
+            model_training_model_metrics_list.append(model_metrics_dict)
+
+        return model_training_manger_info_list, model_training_manager_feature_metrics_lists, model_training_model_metrics_list
