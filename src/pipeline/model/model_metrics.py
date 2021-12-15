bcca9b05c56ef1808e90000b3845f5f3e8df44d6
Elad Prager <elad.prager@gmail.com>
2021-12-15 21:57:26 +0200

    add the following Model Metrics: Precision, Recall, F1, AUC

--- src/pipeline/model/model_metrics.py
+++ src/pipeline/model/model_metrics.py
@@ -14,3 +14,59 @@ class Accuracy(IModelMetric):
     @property
     def mtype(self):
         return self._mtype
+
+
+class Precision(IModelMetric):
+    def __init__(self, value: float):
+        self._value = value
+        self._mtype = ModelMetricType.Precision
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def mtype(self):
+        return self._mtype
+
+
+class Recall(IModelMetric):
+    def __init__(self, value: float):
+        self._value = value
+        self._mtype = ModelMetricType.Recall
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def mtype(self):
+        return self._mtype
+
+
+class F1(IModelMetric):
+    def __init__(self, value: float):
+        self._value = value
+        self._mtype = ModelMetricType.F1
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def mtype(self):
+        return self._mtype
+
+
+class AUC(IModelMetric):
+    def __init__(self, value: float):
+        self._value = value
+        self._mtype = ModelMetricType.AUC
+
+    @property
+    def value(self):
+        return self._value
+
+    @property
+    def mtype(self):
+        return self._mtype
