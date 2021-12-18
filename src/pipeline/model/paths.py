7a3bf584b4357547ea036fd4780dcba4b6ccdf78
Elad Prager <elad.prager@gmail.com>
2021-12-18 20:54:27 +0200

    fix CR comments - part b

--- src/pipeline/model/paths.py
+++ src/pipeline/model/paths.py
@@ -1,5 +1,4 @@
 import os
 
 
-BANK_MARKETING_PRODUCTION_MODEL_PATH = os.path.abspath(os.path.join(__file__, "..", "..", "model", "raw_files",
-                                                                    "raw_files/BankMarketingProductionModel.sav"))
+BANK_MARKETING_PRODUCTION_MODEL_PATH = os.path.abspath(os.path.join(__file__, "..", "..", "model", "raw_files", "BankMarketingProductionModel.sav"))
