6d0b346c7bd9fc8805eef386f9213388285e3358
guyfreund <guyfreund@gmail.com>
2021-12-18 17:49:39 +0200

    test model retraining manager

--- test/model/test_model_retraining_manager.py
+++ test/model/test_model_retraining_manager.py
@@ -1,3 +1,4 @@
+from typing import List
 from unittest import TestCase
 
 from src.pipeline.datasets.training_datasets import BankMarketingDataset
@@ -6,15 +7,26 @@ from src.pipeline.model.production_models import BankMarketingProductionModel
 from src.pipeline.preprocessing.preprocessor import Preprocessor
 
 
-class TestMultipleDatasetModelTrainingManager(TestCase):
+class TestMultipleDatasetModelReTrainingManager(TestCase):
     def setUp(self) -> None:
-        self.bank_marketing_info = ModelTrainingManagerInfo(
-            preprocessor=Preprocessor(),
-            dataset=BankMarketingDataset(),
-            model=BankMarketingProductionModel(),
-        )
+        self.info_list: List[ModelTrainingManagerInfo] = [
+            ModelTrainingManagerInfo(
+                preprocessor=Preprocessor(),
+                dataset=BankMarketingDataset(),
+                model=BankMarketingProductionModel()
+            ),
+            ModelTrainingManagerInfo(
+                preprocessor=Preprocessor(),
+                dataset=BankMarketingDataset(),
+                model=BankMarketingProductionModel(),
+                to_train=False
+            )
+        ]
 
     def test_manage(self):
-        info_list, feature_metrics_list_of_lists, model_metrics_list = MultipleDatasetModelTrainingManager([self.bank_marketing_info]).manage()
+        info_list, feature_metrics_list_of_lists, model_metrics_list = MultipleDatasetModelTrainingManager(self.info_list).manage()
+        assert len(info_list) == 1
+        assert len(feature_metrics_list_of_lists) == 1
+        assert len(model_metrics_list) == 1
         for info, feature_metrics_list, model_metrics_dict in zip(info_list, feature_metrics_list_of_lists, model_metrics_list):
             self.assertEqual(model_metrics_dict, info.model.model_metrics)
