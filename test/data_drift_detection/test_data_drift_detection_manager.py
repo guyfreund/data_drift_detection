3eb54f0cd47da673f37f744e802d73e00af78496
guyfreund <guyfreund@gmail.com>
2021-12-20 21:37:45 +0200

    bug fixes

--- test/data_drift_detection/test_data_drift_detection_manager.py
+++ test/data_drift_detection/test_data_drift_detection_manager.py
@@ -1,4 +1,4 @@
-import pytest
+import logging
 
 from src.pipeline.data_drift_detection.data_drift import DataDrift
 from src.pipeline.data_drift_detection.data_drift_detection_manager import DataDriftDetectionManagerInfo, \
@@ -6,21 +6,27 @@ from src.pipeline.data_drift_detection.data_drift_detection_manager import DataD
 from src.pipeline.datasets.deployment_datasets import BankMarketingDeploymentDatasetPlus, BankMarketingDeploymentDataset
 from src.pipeline.datasets.paths import BANK_MARKETING_TRAINING_PROCESSED_DF_PLUS_PATH, \
     BANK_MARKETING_TRAINING_FEATURE_METRIC_LIST_PATH, BANK_MARKETING_TRAINING_PROCESSED_DF_PATH
-from src.pipeline.datasets.training_datasets import BankMarketingDataset
+from src.pipeline.datasets.training_datasets import BankMarketingDataset, BankMarketingDatasetPlus
 from src.pipeline.model.production_models import BankMarketingProductionModel
 from src.pipeline.preprocessing.preprocessor import Preprocessor
+
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    level='INFO',
+    datefmt='%d-%b-%y %H:%M:%S'
+)
 
 
 class TestDataDriftDetectionManager:
     def test_no_data_drift(self):
         bank_marketing_info = DataDriftDetectionManagerInfo(
-            deployment_dataset_plus=BankMarketingDataset(),
-            training_processed_df_plus_path=BANK_MARKETING_TRAINING_PROCESSED_DF_PLUS_PATH,
-            preprocessor=Preprocessor(),  # TODO: fix
-            model=BankMarketingProductionModel(),  # TODO: fix
             deployment_dataset=BankMarketingDataset(),
+            deployment_dataset_plus=BankMarketingDatasetPlus(),
+            training_processed_df_path=BANK_MARKETING_TRAINING_PROCESSED_DF_PATH,
+            training_processed_df_plus_path=BANK_MARKETING_TRAINING_PROCESSED_DF_PLUS_PATH,
             training_feature_metrics_list_path=BANK_MARKETING_TRAINING_FEATURE_METRIC_LIST_PATH,
-            training_processed_df_path=BANK_MARKETING_TRAINING_PROCESSED_DF_PATH
+            preprocessor=Preprocessor(),
+            model=BankMarketingProductionModel(),  # TODO: fix
         )
         data_drift_detection_manager = MultipleDatasetDataDriftDetectionManager(info_list=[bank_marketing_info])
         data_drift: DataDrift = data_drift_detection_manager.manage()[0]
