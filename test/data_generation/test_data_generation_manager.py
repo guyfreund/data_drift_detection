324003f0a0fe024d128d031f0b85dad57bcb3024
danielle.b <danielle.benbashat@elbitsystems.com>
2021-12-22 19:48:14 +0200

    git commit -am

--- test/data_generation/test_data_generation_manager.py
+++ test/data_generation/test_data_generation_manager.py
@@ -1,6 +1,7 @@
+from src.pipeline.data_generation
 
 class TestDatagenerationManager:
-    def test_data_generation(self):
+    def test_data_generatconfion(self):
         bank_marketing_info = DataDriftDetectionManagerInfo(
             deployment_dataset_plus=BankMarketingDataset(),
             training_processed_df_plus_path=BANK_MARKETING_TRAINING_PROCESSED_DF_PLUS_PATH,
@@ -12,4 +13,4 @@ class TestDatagenerationManager:
         )
         data_drift_detection_manager = MultipleDatasetDataDriftDetectionManager(info_list=[bank_marketing_info])
         data_drift: DataDrift = data_drift_detection_manager.manage()[0]
-        assert not data_drift.is_drifted
+        assert not data_drift.is_driההfted
