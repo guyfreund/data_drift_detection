2197c636d7376ada5fce97af5f64ecba0fcc84d9
guyfreund <guyfreund@gmail.com>
2021-12-25 17:35:09 +0200

    revert preprocess paths

--- src/pipeline/preprocessing/paths.py
+++ src/pipeline/preprocessing/paths.py
@@ -1,5 +1,5 @@
 import os
 
 # Encoding Mapping
-BANK_MARKETING_LABEL_ENCODER_PATH = os.path.abspath(os.path.join(__file__, "..", "preprocessing", "raw_files", "BanMarketingLabelEncoding.npy"))
-GERMAN_CREDIT_LABEL_ENCODER_PATH = os.path.abspath(os.path.join(__file__, "..", "preprocessing", "raw_files", "GermanCreditLabelEncoding.npy"))
+BANK_MARKETING_LABEL_ENCODER_PATH = os.path.abspath(os.path.join(__file__, "..", "..", "preprocessing", "raw_files", "BanMarketingLabelEncoding.npy"))
+GERMAN_CREDIT_LABEL_ENCODER_PATH = os.path.abspath(os.path.join(__file__, "..", "..", "preprocessing", "raw_files", "GermanCreditLabelEncoding.npy"))
