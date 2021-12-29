1d3958c904f3be3ce4c503a9a0e237d8fc98419a
Guy Freund <53565845+guyfreund@users.noreply.github.com>
2021-12-29 19:24:56 +0200

    Delete wow.py

--- wow.py
+++
@@ -1,9 +0,0 @@
-from src.pipeline.datasets.dataset import Dataset
-from src.pipeline.datasets.training_datasets import BankMarketingSampledTrainingTrainDataset
-from src.pipeline.datasets.deployment_datasets import BankMarketingSampledDeploymentDataset
-from src.pipeline.datasets.paths import BANK_MARKETING_RETRAINING_DF
-
-x = Dataset.concatenate(
-    dataset_list=[BankMarketingSampledTrainingTrainDataset(), BankMarketingSampledDeploymentDataset()],
-    path=BANK_MARKETING_RETRAINING_DF
-)
